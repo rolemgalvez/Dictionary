c3457069698f5cb6d5d705c0bbe348b9fa2d9fbb
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-14 15:44:50 +0800

    feature: add DeductionPriority project

---
+++ DeductionPriority/Program.cs
@@ -0,0 +1,24 @@
+﻿//Welcome message
+Console.WriteLine("Deduction Priority");
+Console.WriteLine("------------------");
+Console.WriteLine();
+
+//Declare variables
+Dictionary<int, string> deductions = new Dictionary<int, string>();
+
+deductions.Add(1, "SSS");
+deductions.Add(2, "PHIC");
+deductions.Add(3, "HDMF");
+deductions.Add(4, "Tax");
+
+//Show results
+int ctr = 1;
+while (ctr <= 4)
+{
+    Console.WriteLine($"{ctr}. {deductions[ctr++]}");
+}
+
+//End of program
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
