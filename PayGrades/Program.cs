c2198734fcf1fd3cfe476b04ced86f1e3f2bf327
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-14 17:02:57 +0800

    feature: add PayGrades project

---
+++ PayGrades/Program.cs
@@ -0,0 +1,25 @@
+﻿//Welcome message
+Console.WriteLine("Pay Grades");
+Console.WriteLine("----------");
+Console.WriteLine();
+
+//Prepare variables
+Dictionary<string, decimal> payGrades = new Dictionary<string, decimal>();
+
+payGrades.Add("Level 1", 10000);
+payGrades.Add("Level 2", 20000);
+payGrades.Add("Level 3", 30000);
+payGrades.Add("Level 4", 40000);
+payGrades.Add("Level 5", 50000);
+
+//Show results
+int ctr = 1;
+while (ctr <= 5)
+{
+    Console.WriteLine($"Level {ctr}: {payGrades[$"Level {ctr++}"]}");
+}
+
+//End of program
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
